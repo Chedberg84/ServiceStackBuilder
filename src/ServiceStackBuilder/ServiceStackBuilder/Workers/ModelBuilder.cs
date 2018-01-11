7acff47aaf5b940593f3a8bde4a23fdf33bcdb46
Chris Hedberg <ChristophHedberg@gmail.com>
2018-01-11 12:47:57 -0600

    Update ModelBuilder.cs

--- src/ServiceStackBuilder/ServiceStackBuilder/Workers/ModelBuilder.cs
+++ src/ServiceStackBuilder/ServiceStackBuilder/Workers/ModelBuilder.cs
@@ -82,41 +82,5 @@ namespace ServiceStackBuilder.Workers
             GenericProjectUpdate(project, fileNames);
         }
         
-        private string GenericBuild(string workingDir, string templatePath, string actionVerb, string actionType)
-        {
-            string templatePathFull = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), templatePath);
-            string template = File.ReadAllText(templatePathFull);
-            template = template.Replace("<SolutionName>", UserInput.SolutionName);
-            string fileName = string.Format("{0}{1}{2}", actionVerb, UserInput.obj, actionType, ".cs");
-            File.WriteAllText(Path.Combine(workingDir, fileName), template);
-            
-            return fileName;
-        }
-        
-        private void GenericProjectUpdate(Project project, string[] fileNames)
-        {
-            //Update the project file to include these 2 new files
-            XNamespace msbuild = "http://schemas.microsoft.com/developer/msbuild/2003";
-            string projFile = Path.Combine(UserInput.Root, project.Path);
-            XDocument doc = XDocument.Load(projFile);
-
-            var itemGroups = doc.Root.Elements(msbuild + "ItemGroup");
-            var compile = itemGroups.Where(x => x.Elements(msbuild + "Compile").Count() > 0).FirstOrDefault();
-
-            foreach(var name in fileNames)
-            {
-                string include = "Messages\\" + UserInput.obj + "\\" + name;
-
-                var existing = compile.Elements().Where(x => x.FirstAttribute.Value.Equals(include)).FirstOrDefault();
-                if (existing == null)
-                {
-                    XElement docRequest = new XElement(msbuild + "Compile", new XAttribute("Include", include));
-                    compile.Add(docRequest);
-                }
-            }
-
-            doc.Save(projFile);
-        }
-        
     }
 }
