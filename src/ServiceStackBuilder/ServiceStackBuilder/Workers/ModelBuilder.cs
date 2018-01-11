4783ab8cad6b93afff7b724fdea762ee360a8417
Chris Hedberg <ChristophHedberg@gmail.com>
2018-01-11 11:12:38 -0600

    Update ModelBuilder.cs

--- src/ServiceStackBuilder/ServiceStackBuilder/Workers/ModelBuilder.cs
+++ src/ServiceStackBuilder/ServiceStackBuilder/Workers/ModelBuilder.cs
@@ -28,20 +28,73 @@ namespace ServiceStackBuilder.Workers
             //create request and response files
             Directory.CreateDirectory(workingDir);
 
-            string requestTemplatePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Templates\\CreateRequestTemplate.txt");
-            string requestTemplate = File.ReadAllText(requestTemplatePath);
-            requestTemplate = requestTemplate.Replace("<RequestTemplate>", UserInput.obj + "Request");
-            string requestFileName = string.Format("{0}{1}", UserInput.obj, "Request.cs");
-
-            File.WriteAllText(Path.Combine(workingDir, requestFileName), requestTemplate);
-
-            string responseTemplatePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Templates\\CreateResponseTemplate.txt");
-            string responseTemplate = File.ReadAllText(responseTemplatePath);
-            responseTemplate = responseTemplate.Replace("<ResponseTemplate>", UserInput.obj + "Response");
-            string responseFileName = string.Format("{0}{1}", UserInput.obj, "Response.cs");
-
-            File.WriteAllText(Path.Combine(workingDir, responseFileName), responseTemplate);
-
+            BuildCreate(workingDir, project);
+            BuildRead(workingDir, project);
+            BuildUpdate(workingDir, project);
+            BuildDelete(workingDir, project);
+        }
+        
+        private void BuildCreate(string workingDir, Project project)
+        {
+            string templatePathRequest = "Templates\\Models\\CreateRequestTemplate.txt";
+            string templatePathResponse = "Templates\\Models\\CreateResponseTemplate.txt";
+            
+            string createRequestFileName = GenericBuild(workingDir, templatePathRequest, "Create", "Request");
+            string createResponseFileName = GenericBuild(workingDir, templatePathResponse, "Create", "Response");
+            
+            string[] fileNames = new string[] { createRequestFileName, createResponseFileName };
+            GenericProjectUpdate(project, fileNames);
+        }
+        
+        private void BuildRead(string workingDir, Project project)
+        {
+            string templatePathRequest = "Templates\\Models\\ReadRequestTemplate.txt";
+            string templatePathResponse = "Templates\\Models\\ReadResponseTemplate.txt";
+            
+            string createRequestFileName = GenericBuild(workingDir, templatePathRequest, "Read", "Request");
+            string createResponseFileName = GenericBuild(workingDir, templatePathResponse, "Read", "Response");
+            
+            string[] fileNames = new string[] { createRequestFileName, createResponseFileName };
+            GenericProjectUpdate(project, fileNames);
+        }
+        
+        private void BuildUpdate(string workingDir, Project project)
+        {
+            string templatePathRequest = "Templates\\Models\\UpdateRequestTemplate.txt";
+            string templatePathResponse = "Templates\\Models\\UpdateResponseTemplate.txt";
+            
+            string createRequestFileName = GenericBuild(workingDir, templatePathRequest, "Update", "Request");
+            string createResponseFileName = GenericBuild(workingDir, templatePathResponse, "Update", "Response");
+            
+            string[] fileNames = new string[] { createRequestFileName, createResponseFileName };
+            GenericProjectUpdate(project, fileNames);
+        }
+        
+        private void BuildDelete(string workingDir, Project project)
+        {
+            string templatePathRequest = "Templates\\Models\\DeleteRequestTemplate.txt";
+            string templatePathResponse = "Templates\\Models\\DeleteResponseTemplate.txt";
+            
+            string createRequestFileName = GenericBuild(workingDir, templatePathRequest, "Delete", "Request");
+            string createResponseFileName = GenericBuild(workingDir, templatePathResponse, "Delete", "Response");
+            
+            string[] fileNames = new string[] { createRequestFileName, createResponseFileName };
+            GenericProjectUpdate(project, fileNames);
+        }
+        
+        private string GenericBuild(string workingDir, string templatePath, string actionVerb, string actionType)
+        {
+            string templatePathFull = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), templatePath);
+            string template = File.ReadAllText(templatePathFull);
+            template = template.Replace("<SolutionName>", UserInput.SolutionName);
+            string fileName = string.Format("{0}{1}{2}", actionVerb, UserInput.obj, actionType, ".cs");
+            File.WriteAllText(Path.Combine(workingDir, fileName), template);
+            
+            return fileName;
+        }
+        
+        private void GenericProjectUpdate(Project project, string[] fileNames)
+        {
             //Update the project file to include these 2 new files
             XNamespace msbuild = "http://schemas.microsoft.com/developer/msbuild/2003";
             string projFile = Path.Combine(UserInput.Root, project.Path);
@@ -50,40 +103,20 @@ namespace ServiceStackBuilder.Workers
             var itemGroups = doc.Root.Elements(msbuild + "ItemGroup");
             var compile = itemGroups.Where(x => x.Elements(msbuild + "Compile").Count() > 0).FirstOrDefault();
 
-            string requestInclude = "Messages\\" + UserInput.obj + "\\" + requestFileName;
-            string responseInclude = "Messages\\" + UserInput.obj + "\\" + responseFileName;
-
-            var existingRequest = compile.Elements().Where(x => x.FirstAttribute.Value.Equals(requestInclude)).FirstOrDefault();
-            if (existingRequest == null)
+            foreach(var name in fileNames)
             {
-                XElement docRequest = new XElement(msbuild + "Compile", new XAttribute("Include", requestInclude));
-                compile.Add(docRequest);
-            }
+                string include = "Messages\\" + UserInput.obj + "\\" + name;
 
-            var existingResponse = compile.Elements().Where(x => x.FirstAttribute.Value.Equals(responseInclude)).FirstOrDefault();
-            if (existingResponse == null)
-            {
-                XElement docResponse = new XElement(msbuild + "Compile", new XAttribute("Include", responseInclude));
-                compile.Add(docResponse);
+                var existing = compile.Elements().Where(x => x.FirstAttribute.Value.Equals(include)).FirstOrDefault();
+                if (existing == null)
+                {
+                    XElement docRequest = new XElement(msbuild + "Compile", new XAttribute("Include", include));
+                    compile.Add(docRequest);
+                }
             }
 
             doc.Save(projFile);
         }
         
-        private void BuildCreate()
-        {
-        }
-        
-        private void BuildRead()
-        {
-        }
-        
-        private void BuildUpdate()
-        {
-        }
-        
-        private void BuildDelete()
-        {
-        }
     }
 }
