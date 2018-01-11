e5b72e85a264ad0983f1d1929f25f48ec9369ac8
Chris Hedberg <ChristophHedberg@gmail.com>
2018-01-11 12:46:07 -0600

    Update Builder.cs

--- src/ServiceStackBuilder/ServiceStackBuilder/Workers/Builder.cs
+++ src/ServiceStackBuilder/ServiceStackBuilder/Workers/Builder.cs
@@ -1,1 +1,17 @@
+using Onion.SolutionParser.Parser.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ServiceStackBuilder.Workers
+{
+    public class Builder : IBuilder
+    {
+        public virtual void Go()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
