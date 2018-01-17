4c997ce03ddc6dd4033f3ea089c67085c2aa9088
Chris Hedberg <christophhedberg@gmail.com>
2018-01-16 18:41:53 -0600

    Added lazy loading and saving of results.

--- src/ServiceStackBuilder/ServiceStackBuilder/UserInput.cs
+++ src/ServiceStackBuilder/ServiceStackBuilder/UserInput.cs
@@ -15,31 +15,48 @@ namespace ServiceStackBuilder
         //public static string sln = @"C:\GitHub\CostsApi\src\CostsApi.sln";
         //public static string obj = "BadAss";
 
+        private static string _root;
         public static string Root
         {
             get
             {
-                var slnInfo = new FileInfo(sln);
-                return slnInfo.DirectoryName;
+                if(_root == null)
+                {
+                    var slnInfo = new FileInfo(sln);
+                    _root = slnInfo.DirectoryName;
+                }
+
+                return _root;
             }
         }
 
+        private static string _testDir;
         public static string TestDir
         {
             get
             {
-                var slnInfo = new FileInfo(sln);
-                return Path.Combine(slnInfo.Directory.Parent.FullName, "tests");
+                if(_testDir == null)
+                {
+                    var slnInfo = new FileInfo(sln);
+                    _testDir = Path.Combine(slnInfo.Directory.Parent.FullName, "tests");
+                }
+
+                return _testDir;
             }
         }
 
+        private static string _solutionName;
         public static string SolutionName
         {
             get
             {
-                var slnInfo = new FileInfo(sln);
-                var name = slnInfo.Name.Substring(0, slnInfo.Name.Length - 4);
-                return name;
+                if(_solutionName == null)
+                {
+                    var slnInfo = new FileInfo(sln);
+                    _solutionName = slnInfo.Name.Substring(0, slnInfo.Name.Length - 4);
+                }
+
+                return _solutionName;
             }
         }
 
